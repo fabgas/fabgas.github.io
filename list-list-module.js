e92e17f54329d22e4eb8459ccc614f605710a518
fabrice.gastineau <fabrice.gastineau@gmail.com>
2018-11-10 19:31:05 +0100

    ionic temporaire

---
+++ list-list-module.js
@@ -0,0 +1,220 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["list-list-module"],{
+
+/***/ "./src/app/list/list.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/list/list.module.ts ***!
+  \*************************************/
+/*! exports provided: ListPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ListPageModule", function() { return ListPageModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/index.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
+/* harmony import */ var _list_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./list.page */ "./src/app/list/list.page.ts");
+/* harmony import */ var _services_projet_service__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./services/projet-service */ "./src/app/list/services/projet-service.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var ListPageModule = /** @class */ (function () {
+    function ListPageModule() {
+    }
+    ListPageModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_2__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_3__["IonicModule"],
+                _angular_common_http__WEBPACK_IMPORTED_MODULE_5__["HttpClientModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forChild([
+                    {
+                        path: '',
+                        component: _list_page__WEBPACK_IMPORTED_MODULE_6__["ListPage"]
+                    }
+                ])
+            ],
+            declarations: [_list_page__WEBPACK_IMPORTED_MODULE_6__["ListPage"]],
+            providers: [_services_projet_service__WEBPACK_IMPORTED_MODULE_7__["ProjetService"]]
+        })
+    ], ListPageModule);
+    return ListPageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/list/list.page.html":
+/*!*************************************!*\
+  !*** ./src/app/list/list.page.html ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n\n<ion-header>\n  <ion-toolbar>\n    <ion-buttons slot=\"start\">\n      <ion-menu-button></ion-menu-button>\n    </ion-buttons>\n    <ion-title>\n      Projets\n    </ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n  <ion-list *ngIf=\"projets\">\n    <ion-item  *ngFor=\"let projet of projets\" (click)=\"navigate(projet)\">\n      {{projet.llProjet}}\n      <div class=\"item-note\" slot=\"end\">\n        {{projet.ucProjet}}\n      </div>\n    </ion-item>\n  </ion-list>\n  <!--\n    <div *ngIf=\"selectedItem\" padding>\n      You navigated here from <b>{{selectedItem.title }}</b>\n    </div>\n  -->\n</ion-content>\n"
+
+/***/ }),
+
+/***/ "./src/app/list/list.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/list/list.page.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/list/list.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/list/list.page.ts ***!
+  \***********************************/
+/*! exports provided: ListPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ListPage", function() { return ListPage; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _services_projet_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./services/projet-service */ "./src/app/list/services/projet-service.ts");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var ListPage = /** @class */ (function () {
+    function ListPage(projetService, router) {
+        this.projetService = projetService;
+        this.router = router;
+        this.icons = [
+            'flask',
+            'wifi',
+            'beer',
+            'football',
+            'basketball',
+            'paper-plane',
+            'american-football',
+            'boat',
+            'bluetooth',
+            'build'
+        ];
+        this.items = [];
+        this.projets = [];
+        for (var i = 1; i < 11; i++) {
+            this.items.push({
+                title: 'Item ' + i,
+                note: 'This is item #' + i,
+                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+            });
+        }
+    }
+    ListPage.prototype.ngOnInit = function () {
+        var _this = this;
+        this.projetService.getProjets().subscribe(function (results) {
+            _this.projets = results;
+        });
+    };
+    // add back when alpha.4 is out
+    ListPage.prototype.navigate = function (projet) {
+        var ucProjet = projet.ucProjet;
+        var llProjet = projet.llProjet;
+        this.router.navigate(['process/', { ucProjet: ucProjet, llProjet: llProjet }]);
+    };
+    ListPage = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-list',
+            template: __webpack_require__(/*! ./list.page.html */ "./src/app/list/list.page.html"),
+            styles: [__webpack_require__(/*! ./list.page.scss */ "./src/app/list/list.page.scss")]
+        }),
+        __metadata("design:paramtypes", [_services_projet_service__WEBPACK_IMPORTED_MODULE_1__["ProjetService"], _angular_router__WEBPACK_IMPORTED_MODULE_2__["Router"]])
+    ], ListPage);
+    return ListPage;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/list/services/projet-service.ts":
+/*!*************************************************!*\
+  !*** ./src/app/list/services/projet-service.ts ***!
+  \*************************************************/
+/*! exports provided: ProjetService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ProjetService", function() { return ProjetService; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! rxjs */ "./node_modules/rxjs/_esm5/index.js");
+/* harmony import */ var _model_projet__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../model/projet */ "./src/app/list/model/projet.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var ProjetService = /** @class */ (function () {
+    function ProjetService(http) {
+        this.http = http;
+        this.projets = [];
+        var p1 = new _model_projet__WEBPACK_IMPORTED_MODULE_3__["Projet"]("KNFR124801", "Nantes Nord");
+        var p2 = new _model_projet__WEBPACK_IMPORTED_MODULE_3__["Projet"]("KNFR124802", "Nantes Ouest");
+        this.projets[0] = p1;
+        this.projets[1] = p2;
+    }
+    ProjetService.prototype.getProjets = function () {
+        console.log(this.projets);
+        return Object(rxjs__WEBPACK_IMPORTED_MODULE_2__["of"])(this.projets);
+    };
+    ProjetService.prototype.getProjet = function (ucProjet) {
+        return this.projets.find(function (projet) { return projet.ucProjet === ucProjet; });
+    };
+    ProjetService = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Injectable"])({
+            providedIn: 'root'
+        }),
+        __metadata("design:paramtypes", [_angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClient"]])
+    ], ProjetService);
+    return ProjetService;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=list-list-module.js.map
